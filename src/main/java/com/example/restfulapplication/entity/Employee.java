05d85da8a185d7ebb3a40e3a60948e7a28d3c4b9
Albert <fruitlesskey2@mail.ru>
2023-07-07 19:04:48 +0300

    Add Layers

---
+++ src/main/java/com/example/restfulapplication/entity/Employee.java
@@ -0,0 +1,27 @@
+package com.example.restfulapplication.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+@Entity
+@Table(name = "employee", schema = "public")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Employee implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+
+}
---
+++ src/main/java/com/example/restfulapplication/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.example.restfulapplication.repository;
+
+import com.example.restfulapplication.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
